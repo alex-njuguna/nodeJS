e8a764ac2cde98a114843673909dd7abeefe22d9
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-19 00:15:01 +0300

    event loop examples(setInterval)

---
+++ node tutorial/1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run first')
+// process stays alive unlee we kill the process CONTROL + C or we encounter an unexpected error
